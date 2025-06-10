322e54594b26fb6d0a744b5540754942b0940bc9
Muhammad Conn <126736146+pycoder08@users.noreply.github.com>
2025-06-10 16:16:24 -0700

    Completed the requested methods and created a Dice class. Added test cases to Main

---
+++ Dice.java
@@ -0,0 +1,62 @@
+public class Dice {
+    /* INSTANCE VARIABLES */
+    private int rollNum;
+
+    /**
+     * Returns string representation of the dice object
+     * @return String representation of the die
+     */
+    public String toString() {
+        return "Dice roll number: " + rollNum;
+    }
+
+    /**
+     * Sets the rolled number value of a dice object
+     * @param rollNum number to set rolled value to
+     */
+    public void setRollNum(int rollNum) {
+        this.rollNum = rollNum;
+    }
+
+    /**
+     * returns current rolled number
+     * @return rolled number
+     */
+    public int getRollNum() {
+        return rollNum;
+    }
+
+    /**
+     * Default constructor for the dice class
+     */
+    public Dice() {
+        this.rollNum = 0; // 0 for default
+    }
+
+    /**
+     * Full constructor for the dice class
+     * @param rollNum number to set the rolled number to
+     */
+    public Dice(int rollNum) {
+        this.rollNum = rollNum; 
+    }
+
+    /**
+     * Copy constructor for the dice class
+     * @param other other dice object to copy
+     */
+    public Dice(Dice other) {
+        this.rollNum = other.rollNum;
+    }
+
+    /**
+     * Equals method for the dice class
+     * @param other dice object to compare source die to
+     * @return true of the dice are equal, false otherwise
+     */
+    public boolean equals(Dice other) {
+        return this.rollNum == other.rollNum;
+    }
+
+    
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,102 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+
+        //TESTING//
+        // Test the printVoid method for input 5 
+        printVoid(5);
+
+        System.out.println();
+
+        // Test the nextPrime method for input 45 (should print 47)
+        int primeInput = 45;
+        System.out.println("Next prime after " + primeInput + ": " + nextPrime(primeInput));
+
+        System.out.println();
+
+        // Default die
+        Dice defaultDie = new Dice();
+        System.out.println("Default die:");
+        System.out.println(defaultDie.toString());
+
+        System.out.println();
+
+        //  Die #1 made with full constructor
+        Dice fullDie1 = new Dice(6);
+        System.out.println("Full constructor die:");
+        System.out.println(fullDie1.toString());
+
+        System.out.println();
+
+        //  Die #2 made with full constructor
+        Dice fullDie2 = new Dice(8);
+        System.out.println("Die to be copied:");
+        System.out.println(fullDie2.toString());
+
+        System.out.println();
+
+        // Die #3 made using copy constructor
+        Dice copyDie = new Dice(fullDie2);
+        System.out.println("Copied die:");
+        System.out.println(copyDie.toString());
+
+    }
+
+    /**
+     * Prints a simple statement for each iteration in a loop 
+     * @param printTimes How many iterations the loop should run for
+     */
+    public static void printVoid(int printTimes)
+    {
+        for (int iteration = 0; iteration < printTimes; iteration++)
+        {
+            System.out.println("Iteration: " + (iteration + 1));
+        }
+    }
+
+    /**
+     * Returns the next prime number for a given input from 0 to 100.
+     * @param input An integer input from 0 to 100
+     * @return The next prime number greater than the input
+     */
+    public static int nextPrime(int input)
+    {
+        int[] primes = new int[26];
+        primes[0] = 2;
+        primes[1] = 3;
+        primes[2] = 5;
+        primes[3] = 7;
+        primes[4] = 11;
+        primes[5] = 13;
+        primes[6] = 17;
+        primes[7] = 19;
+        primes[8] = 23;
+        primes[9] = 29;
+        primes[10] = 31;
+        primes[11] = 37;
+        primes[12] = 41;
+        primes[13] = 43;
+        primes[14] = 47;
+        primes[15] = 53;
+        primes[16] = 59;
+        primes[17] = 61;
+        primes[18] = 67;
+        primes[19] = 71;
+        primes[20] = 73;
+        primes[21] = 79;
+        primes[22] = 83;
+        primes[23] = 89;
+        primes[24] = 97;
+        primes[25] = 101;
+        
+        for (int prime = 0; prime < primes.length; prime++)
+        {
+            if (primes[prime] > input)
+            {
+                return primes[prime];
+            }
+        }
+        return -1; // Return -1 if no prime found
+
     }
 }
